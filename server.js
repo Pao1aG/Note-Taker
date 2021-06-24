481bdfc49a3669af126ffccb369bf18155594aaf
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-06-23 22:00:35 -0700

    added notes to self

--- server.js
+++ server.js
@@ -26,53 +26,74 @@ app.get("/notes", function (req, res) {
 
 // GET - /api/notes
 app.get("/api/notes", async function (req, res) {
-    try {
-         console.log("hello! im triggered (get)")
-        const data = await fs.readFile("./db/db.json", "utf8");
-        res.json(JSON.parse(data));
-        //console.log("This is at the get" + data);//shows object within array in db.json
-    } catch (err) {
-        res.status(500).end("Server failed at get");
-    }
+  try {
+      // console.log("hello! im triggered (get)")
+      const data = await fs.readFile("./db/db.json", "utf8");
+      res.json(JSON.parse(data));
+      //console.log("This is at the get" + data);//shows object within array in db.json
+  } catch (err) {
+      res.status(500).end("Server failed at get");
+  }
 });
 
+// GET - /api/notes/:id NEED TO HAVE EACH SAVED NOTE TO HAVE THEIR OWN ID
+// app.get("/api/notes/:id", async function (req, res) {
+//   try{
+//     const data = await fs.readFile("")
+//   }
+// });
+
+//NEED TO ASSIGN IDS TO EACH NOTE TO HAVE IT DISPLAY WHEN CLICKED
 // POST - /api/notes
 app.post("/api/notes", async function (req, res) {
-    try {
-      console.log("hello! im triggered (post)")
-        const newNote = req.body;
-        console.log(newNote); //shows new object
+  try {
+    // console.log("hello! im triggered (post)")
+      const newNote = req.body;
+      console.log(newNote); //shows new object
 
-        const data = await fs.readFile("./db/db.json", "utf8");
-        //Parsing the data from json file, and then stringifying it 
-        let str = JSON.stringify(JSON.parse(data));
-        console.log("str original: " + str)
-        //Substring takes in the start and the end, and returns what's in-between
-        // Starting at index 0, and then cutting off the ] at end
-        str = str.substring(0, str.length - 1);
-        console.log("str after:    " + str)
+      const data = await fs.readFile("./db/db.json", "utf8");
+      //Parsing the data from json file, and then stringifying it 
+      let str = JSON.stringify(JSON.parse(data)); //found at t.ly/mscR
+      // console.log("str original: " + str)
+      //Substring takes in the start and the end, and returns what's in-between
+      // Starting at index 0, and then cutting off the ] at end
+      str = str.substring(0, str.length - 1);
+      // console.log("str after:    " + str)
 
-        // Making the newly added note into a string
-        const stringNote = JSON.stringify(newNote);
-        console.log("str note:     " + stringNote)
+      // Making the newly added note into a string
+      const stringNote = JSON.stringify(newNote);
+      // console.log("str note:     " + stringNote)
 
-        //Concatenating the old and new data
-        console.log("str append:   " + `${str},${stringNote}]`)
-        await fs.writeFile("./db/db.json", `${str},${stringNote}]`);
-
-
-        //Make window reload after post
-        window.location.reload();
+      //Concatenating the old and new data
+      console.log("str append:   " + `${str},${stringNote}]`)
+      await fs.writeFile("./db/db.json", `${str},${stringNote}]`);
 
 
-    } catch (err) {
-        res.status(500).end("Server failed at post")
-    }
+      //Make window reload after post
+      window.location.reload();
+
+
+  } catch (err) {
+      res.status(500).end("Server failed at post");
+  }
 });
 
-// DELETE - /api/notes
+// DELETE - /api/notes First need to have the GET working 
+// app.delete("/api/notes", async function (req, res) {
+//   try {
+//     console.log("Hello! I am triggered (delete)")
+//     // const data = await fs.readFile("./db/db.json", "utf8");
+//     // res.json(JSON.parse(data));
+//     // //Parsing the data from json file, and then stringifying it 
+//     // let str = JSON.stringify(JSON.parse(data));
+//     // console.log(str);
 
 
+//   } catch (err) {
+//     res.status(500).end("Server failed at delete");
+//   }
+
+// });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
